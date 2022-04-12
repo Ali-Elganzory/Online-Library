0c74bb5f2ab0d0c39bb77e2897c0723a7d804815
Ali Elganzory <alielganzory@hotmail.com>
2022-04-12 21:16:47 +0200

    Moved components to partials inside views.

--- controllers/topnavigator.php
+++ controllers/topnavigator.php
@@ -1,3 +1,3 @@
 <?php
 
-require 'components/topnavigator/topnavigator.view.php';
+require 'partials/topnavigator/topnavigator.view.php';
--- views/partials/topnavigator/topnavigator.view.php
+++ views/partials/topnavigator/topnavigator.view.php
@@ -13,15 +13,15 @@
     <a class="active" href="#favourites">Find Book</a>
 
     <div style="width: 30px; height: 30px;float: right; padding: 5px">
-        <input class="icons" type="image" src="../../assets/png-clipart-computer-icons-hamburger-button-dots-kebab-menu-text-rectangle-thumbnail.png" alt="Search">
+        <input class="icons" type="image" src="../../../assets/png-clipart-computer-icons-hamburger-button-dots-kebab-menu-text-rectangle-thumbnail.png" alt="Search">
     </div>
     <div style="width: 30px; height: 30px;float: right; padding: 5px">
-        <input class="icons" type="image" src="../../assets/index.png" alt="Search">
+        <input class="icons" type="image" src="../../../assets/index.png" alt="Search">
     </div>
 
     <form action="search_books.php">
         <input type="search" placeholder="Find Book..." name="book-search" id="bsearch">
-        <input type="image" src="../../assets/search-icon-svg-28.png" alt="Search">
+        <input type="image" src="../../../assets/search-icon-svg-28.png" alt="Search">
     </form>
 </div>
 
